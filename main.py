aa7be23c2b98fe660e657f666ea636421a421528
quiquemonroy <quiquemonroy@gmail.com>
2025-05-14 21:48:31 +0200

    update secrets

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DB_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
